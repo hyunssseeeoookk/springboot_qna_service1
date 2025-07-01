f92bc2c588680e792f25bbef71899277d150c800
hyunssseeeoookk <jeong.hs059@gmail.com>
2025-07-02 03:09:39 +0900

    최초 업로드

---
+++ src/main/java/com/test/qna_service1/QnaService1Application.java
@@ -0,0 +1,13 @@
+package com.test.qna_service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/qna_service1/QnaService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.qna_service1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
